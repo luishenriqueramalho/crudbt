6bc06c4553777d0b3592754bc7328ce8814cdfa5
Luis Henrique <52759764+luishenriqueramalho@users.noreply.github.com>
2023-08-21 14:09:48 -0300

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify";
 import { AtletasRoutes } from "./routes/atletas";
+import { ArenasRoutes } from "./routes/arenas";
 
 const app = fastify();
 
 app.register(AtletasRoutes);
+app.register(ArenasRoutes);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("Servidor ligado");
+app.listen({ port: 3333 }).then((dev) => {
+  console.log("Servidor ligado", dev);
 });
